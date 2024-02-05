3db9043e8dd1a96f9826a4a59c2ae2d1d1ac9407
Naresh Kumar D <susrisha@gmail.com>
2024-02-05 15:46:25 +0530

    Update OSMConnection.kt
    updates

--- shared/src/commonMain/kotlin/com/example/osmapi/core/OSMConnection.kt
+++ shared/src/commonMain/kotlin/com/example/osmapi/core/OSMConnection.kt
@@ -1,14 +1,16 @@
 package com.example.osmapi.core
 
 
-class OSMConnection(private  val apiUrl: String,
-                    private val userAgent: String,
-                    private  var oauthToken: String? = null,
+class OSMConnection(private var apiUrl: String,
+                    private var userAgent: String,
+                    private var oauthToken: String? = null,
                     private var timeout: Int = DEFAULT_TIMEOUT) {
 
     fun setTimeout(timeout: Int){
         this.timeout = timeout
     }
+
+
 
     companion object{
         private  const val DEFAULT_TIMEOUT: Int = 45*1000
