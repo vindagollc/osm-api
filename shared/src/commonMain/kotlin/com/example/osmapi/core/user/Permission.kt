99ce5a696930a8ff3c769555d07bad0b2353d274
Naresh Kumar D <susrisha@gmail.com>
2024-02-16 16:28:52 +0530

    Permissions api included
    Added permission api and parser for the same.

--- shared/src/commonMain/kotlin/com/example/osmapi/core/user/Permission.kt
+++ shared/src/commonMain/kotlin/com/example/osmapi/core/user/Permission.kt
@@ -1,11 +1,13 @@
 package com.example.osmapi.core.user
 
-enum class Permission {
-    READ_PREFERENCES_AND_USER_DETAILS,
-    CHANGE_PREFERENCES,
-    WRITE_DIARY,
-    MODIFY_MAP,
-    READ_GPS_TRACES,
-    WRITE_NOTES
- // Will need to write the values later.
+/** Simply some constants for known permissions. See
+ *  https://github.com/openstreetmap/openstreetmap-website/blob/master/db/structure.sql#L1108 */
+object Permission {
+    const val READ_PREFERENCES_AND_USER_DETAILS = "allow_read_prefs"
+    const val CHANGE_PREFERENCES = "allow_write_prefs"
+    const val WRITE_DIARY = "allow_write_diary"
+    const val MODIFY_MAP = "allow_write_api"
+    const val READ_GPS_TRACES = "allow_read_gpx"
+    const val WRITE_GPS_TRACES = "allow_write_gpx"
+    const val WRITE_NOTES = "allow_write_notes"
 }
